d2d12f98eb15486bd4f8270f5dbd577a2544f2b9
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2022-04-20 13:07:24 +0200

    [+] New bar + refactoring

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -15,13 +15,13 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 count=$(xrandr | grep " connected" | wc -l)
 
 if [ $count -eq 2 ]; then
-    polybar catppuccin -c ~/.config/polybar/polybar.ini &
-    polybar catppuccin2 -c ~/.config/polybar/polybar.ini &
+    polybar catppuccin -c ~/.config/polybar/polybar2.ini &
+    polybar catppuccin2 -c ~/.config/polybar/polybar2.ini &
 else
     hdmi=$(xrandr | grep "HDMI-1 connected")
     if [ -n "$hdmi" ]; then
-        polybar catppuccin -c ~/.config/polybar/polybar.ini &
+        polybar catppuccin -c ~/.config/polybar/polybar2.ini &
     else
-        polybar catppuccin3 -c ~/.config/polybar/polybar.ini &
+        polybar catppuccin3 -c ~/.config/polybar/polybar2.ini &
     fi
 fi
