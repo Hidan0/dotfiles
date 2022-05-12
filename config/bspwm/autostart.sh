25613b121a8a70e0a410ee51e46adddfee4e5c1d
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2022-05-12 16:23:59 +0200

    [+] dunst

--- config/bspwm/autostart.sh
+++ config/bspwm/autostart.sh
@@ -29,7 +29,8 @@ numlockx on &
 blueberry-tray &
 picom --experimental-backends --config $HOME/.config/bspwm/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-/usr/lib/xfce4/notifyd/xfce4-notifyd &
+# /usr/lib/xfce4/notifyd/xfce4-notifyd &
+dunst &
 # run volumeicon &
 flameshot &
 
