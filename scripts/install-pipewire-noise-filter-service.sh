6fe0f6b58bef1ffeb911dd160b7a02a7f0adbee5
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2022-05-16 12:45:47 +0200

    [M] auto start noise filter services

--- scripts/install-pipewire-noise-filter-service.sh
+++ scripts/install-pipewire-noise-filter-service.sh
@@ -4,3 +4,6 @@
 # is required
 
 cp -i $HOME/.dotfiles/config/pipewire/pipewire-input-filter-chain.service $HOME/.config/systemd/user/
+systemctl --user deamon-reload
+systemctl --user enable pipewire-input-filter-chain.service
+systemctl --user status pipewire-input-filter-chain.service
