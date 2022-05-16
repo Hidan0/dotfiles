8d1ed4d680bef3187673a973454783ed47cf9ed5
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2022-05-16 13:11:35 +0200

    [!] Fix typo

--- scripts/install-pipewire-noise-filter-service.sh
+++ scripts/install-pipewire-noise-filter-service.sh
@@ -4,6 +4,7 @@
 # is required
 
 cp -i $HOME/.dotfiles/config/pipewire/pipewire-input-filter-chain.service $HOME/.config/systemd/user/
-systemctl --user deamon-reload
+systemctl --user daemon-reload
 systemctl --user enable pipewire-input-filter-chain.service
+systemctl --user start pipewire-input-filter-chain.service
 systemctl --user status pipewire-input-filter-chain.service
