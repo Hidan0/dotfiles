883f9f33933c6803643416657089c908fc3f8a04
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2022-05-31 12:25:26 +0200

    [-] useless fonts

--- scripts/install-fonts-v1.sh
+++ scripts/install-fonts-v1.sh
@@ -1,36 +1,25 @@
 #! /bin/bash
 
-yay -S adobe-base-14-fonts
-yay -S adobe-source-han-sans-cn-fonts
-yay -S adobe-source-han-sans-jp-fonts
-yay -S adobe-source-han-sans-kr-fonts   
-yay -S aadobe-source-sans-fonts
 yay -S arcolinux-fonts-git
-yay -S awesome-terminal-fonts
-yay -S gnome-font-viewer
 yay -S gsfonts
-yay -S nerd-fonts-source-code-pro
 yay -S noto-fonts
 yay -S terminus-font
-yay -S ttf-font-awesome
 yay -S ttf-ms-fonts
-yay -S ttf-nerd-fonts-symbols-mono
 yay -S ttf-ubuntu-font-family
-yay -S ttf-wps-fonts
 yay -S urxvt-resize-font-git
 yay -S xorg-mkfontscale
 
 yay -S ttf-dejavu
-yay -S ttf-droid
 yay -S ttf-hack
 yay -S ttf-jetbrains-mono
 yay -S ttf-roboto
 yay -S ttf-roboto-mono
-yay -S ttf-windows
 yay -S ttf-iosevka-nerd
 
 yay -S noto-fonts-emoji
 yay -S ttf-symbola
+
+yay -S nerd-fonts-complete
 
 echo "Done."
 
