d05167e04fd11e0976de56bef4341838a63fc85b
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2022-05-31 12:22:26 +0200

    [!] font problems

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-chosen=$(printf " Power Off\n Restart\n Suspend\n Log out" | rofi -dmenu -i)
+chosen=$(printf " Power Off\n Restart\n鈴 Suspend\n Log out" | rofi -dmenu -i)
 
 case "$chosen" in
-	" Power Off") systemctl poweroff;;
-	" Restart") systemctl reboot;;
-	" Suspend") systemctl suspend;;
-  " Log out") bspc quit;;
+	" Power Off") systemctl poweroff;;
+	" Restart") systemctl reboot;;
+	"鈴Suspend") systemctl suspend;;
+  " Log out") bspc quit;;
 	*) exit 1 ;;
 esac
