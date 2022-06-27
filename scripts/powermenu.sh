3e3ff0768f6ca645ffb1755bbfeda0e5024e24ea
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2022-06-27 23:45:41 +0200

    [FIX]

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -5,7 +5,7 @@ chosen=$(printf " Power Off\n Restart\n鈴 Suspend\n Log out" | rofi -d
 case "$chosen" in
 	" Power Off") systemctl poweroff;;
 	" Restart") systemctl reboot;;
-	"鈴Suspend") systemctl suspend;;
+	"鈴 Suspend") systemctl suspend;;
   " Log out") bspc quit;;
 	*) exit 1 ;;
 esac
