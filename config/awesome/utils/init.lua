5266c3bbd051688a25b5f2fde4e3ab5c18b25de2
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2022-11-11 19:16:35 +0100

    [ADD] function for colorizing text

--- config/awesome/utils/init.lua
+++ config/awesome/utils/init.lua
@@ -1,5 +1,9 @@
 local _M = {}
 
+_M.colorize_markup = function(text, color)
+  return "<span foreground='" .. color .. "'>" .. text .. "</span>"
+end
+
 _M.volume = require("utils.volume")()
 _M.brightness = require("utils.brightness")()
 
