857eb5c1b12a664b9c77d42cd3a9b0fd862955f6
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2022-12-13 16:58:40 +0100

    [ADD] wireplumber conf

---
+++ config/wireplumber/main.lua.d/51-hdmi-output-disable-2.lua
@@ -0,0 +1,12 @@
+rule = {
+	matches = {
+		{
+			{ "node.name", "equals", "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp_4__sink" },
+		},
+	},
+	apply_properties = {
+		["node.disabled"] = true,
+	}
+}
+
+table.insert(alsa_monitor.rules, rule)
