2a1ec4efb1a0401d486cf3c33194ba57bf4dffb3
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-04-14 12:58:17 +0200

    [FIX] updated node names

--- config/wireplumber/main.lua.d/51-hdmi-output-disable-3.lua
+++ config/wireplumber/main.lua.d/51-hdmi-output-disable-3.lua
@@ -1,12 +1,12 @@
 rule = {
 	matches = {
 		{
-			{ "node.name", "equals", "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp_5__sink" },
+			{ "node.name", "equals", "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__HDMI3__sink" },
 		},
 	},
 	apply_properties = {
 		["node.disabled"] = true,
-	}
+	},
 }
 
 table.insert(alsa_monitor.rules, rule)
--- config/wireplumber/main.lua.d/51-main-output-rename.lua
+++ config/wireplumber/main.lua.d/51-main-output-rename.lua
@@ -1,13 +1,17 @@
 rule = {
 	matches = {
 		{
-			{ "node.name", "equals", "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink" },
+			{
+				"node.name",
+				"equals",
+				"alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Headphones__sink",
+			},
 		},
 	},
 	apply_properties = {
 		["node.description"] = "System speaker",
 		["node.nick"] = "System",
-	}
+	},
 }
 
 table.insert(alsa_monitor.rules, rule)
