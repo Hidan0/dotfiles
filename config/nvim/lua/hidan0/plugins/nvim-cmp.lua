b4b3f8c4c388c2d5553e629cba1dd11be3b67a8f
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-09-23 15:56:40 +0200

    refactoring, used var instead of require

--- config/nvim/lua/hidan0/plugins/nvim-cmp.lua
+++ config/nvim/lua/hidan0/plugins/nvim-cmp.lua
@@ -47,7 +47,7 @@ return {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-          elseif require("luasnip").expand_or_jumpable() then
+          elseif luasnip.expand_or_jumpable() then
             vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
           else
             fallback()
@@ -59,7 +59,7 @@ return {
         ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
-          elseif require("luasnip").jumpable(-1) then
+          elseif luasnip.jumpable(-1) then
             vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
           else
             fallback()
