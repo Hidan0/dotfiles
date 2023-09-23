56e42a79f7277e8a241b230345906fdf6652857c
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-09-23 15:21:28 +0200

    rem, rustfmt

--- config/nvim/lua/hidan0/plugins/lsp/null-ls.lua
+++ config/nvim/lua/hidan0/plugins/lsp/null-ls.lua
@@ -29,7 +29,6 @@ return {
             return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" }) -- only enable if root has .eslintrc.js or .eslintrc.cjs
           end,
         }),
-        formatting.rustfmt,
       },
       -- configure format on save
       on_attach = function(current_client, bufnr)
