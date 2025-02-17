9974a630e5e21c48f7fc7b361e5eef7425080ab2
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2025-02-17 22:31:18 +0100

    feat(hypr): switched to catppuccin

--- config/hypr/scripts/start.sh
+++ config/hypr/scripts/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
 
-_ps=(nm-applet waybar mako udiskie hypridle hyprpaper polkit-kde-authentication-agent-1)
+_ps=(nm-applet waybar mako udiskie hypridle hyprpaper)
 for _prs in "${_ps[@]}"; do
 	if [[ `pidof ${_prs}` ]]; then
 		killall -9 ${_prs}
@@ -10,7 +10,6 @@ done
 nm-applet --indicator &
 mako &
 exec waybar -c $HOME/.config/hypr/waybar/config.json -s $HOME/.config/hypr/waybar/style.css &
-/usr/lib/polkit-kde-authentication-agent-1 &
 udiskie &
 hypridle &
 hyprpaper &
