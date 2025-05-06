c3018359642a9fa3ff3b2205732057e4da0ac3a1
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2025-05-06 10:50:03 +0200

    feat(nvim): added cmds for rust lsp

--- config/nvim/lua/hidan0/plugins/lsp/rustaceanvim.lua
+++ config/nvim/lua/hidan0/plugins/lsp/rustaceanvim.lua
@@ -6,12 +6,39 @@ return {
     vim.g.rustaceanvim = {
       server = {
         on_attach = function(_, _)
-          vim.keymap.set("n", "<leader>ca", function()
+          local keymap = vim.keymap
+          local opts = { noremap = true, silent = true }
+
+          keymap.set("n", "gD", function()
+            vim.lsp.buf.declaration()
+          end, opts) -- got to declaration
+          keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- see definition and make edits in window
+          keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- go to implementation
+          keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts) -- go to references
+          keymap.set("n", "<leader>rn", function()
+            vim.lsp.buf.rename()
+          end, opts) -- smart rename
+          keymap.set("n", "]d", function()
+            vim.diagnostic.jump({ count = 1, float = true })
+          end, opts) -- jump to previous diagnostic in buffer
+          keymap.set("n", "[d", function()
+            vim.diagnostic.jump({ count = -1, float = true })
+          end, opts) -- jump to next diagnostic in buffer
+          keymap.set("n", "K", function()
+            vim.lsp.buf.hover()
+          end, opts) -- show documentation for what is under cursor
+          keymap.set("i", "<C-h>", function()
+            vim.lsp.buf.signature_help()
+          end, opts)
+          keymap.set("n", "<leader>wS", "<cmd>Telescope lsp_workspace_symbols<CR>", opts)
+          keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+
+          keymap.set("n", "<leader>ca", function()
             vim.cmd.RustLsp({ "hover", "actions" })
-          end)
-          vim.keymap.set("n", "<leader>d", function()
+          end, opts)
+          keymap.set("n", "<leader>d", function()
             vim.cmd.RustLsp({ "renderDiagnostic", "cycle" })
-          end)
+          end, opts)
         end,
         default_settings = {
           ["rust-analyzer"] = {
