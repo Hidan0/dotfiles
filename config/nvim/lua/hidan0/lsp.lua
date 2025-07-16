3ccf7152ff18fade0d79a707413529cd45299fb0
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2025-07-16 22:32:43 +0200

    fix(nvim): removed deprecated sign_define

--- config/nvim/lua/hidan0/lsp.lua
+++ config/nvim/lua/hidan0/lsp.lua
@@ -169,10 +169,16 @@ vim.lsp.enable({
 })
 
 local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = signs.Error,
+      [vim.diagnostic.severity.WARN] = signs.Warn,
+      [vim.diagnostic.severity.HINT] = signs.Hint,
+      [vim.diagnostic.severity.INFO] = signs.Info,
+    },
+  },
+})
 
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
 ---@diagnostic disable-next-line: duplicate-set-field
