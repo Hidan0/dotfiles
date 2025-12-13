ea683763e63d6486802b25cbf22d6b9c5b473924
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2025-12-13 06:00:26 +0100

    nvim(nvim-ufo): remaps + number of folded lines

--- config/nvim/lua/hidan0/plugins/syntax.lua
+++ config/nvim/lua/hidan0/plugins/syntax.lua
@@ -55,9 +55,42 @@ return {
                 lineFoldingOnly = true,
             }
 
+            -- To show number of folded lines
+            local handler = function(virtText, lnum, endLnum, width, truncate)
+                local newVirtText = {}
+                local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+                local sufWidth = vim.fn.strdisplaywidth(suffix)
+                local targetWidth = width - sufWidth
+                local curWidth = 0
+                for _, chunk in ipairs(virtText) do
+                    local chunkText = chunk[1]
+                    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                    if targetWidth > curWidth + chunkWidth then
+                        table.insert(newVirtText, chunk)
+                    else
+                        chunkText = truncate(chunkText, targetWidth - curWidth)
+                        local hlGroup = chunk[2]
+                        table.insert(newVirtText, { chunkText, hlGroup })
+                        chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                        -- str width returned from truncate() may less than 2nd argument, need padding
+                        if curWidth + chunkWidth < targetWidth then
+                            suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+                        end
+                        break
+                    end
+                    curWidth = curWidth + chunkWidth
+                end
+                table.insert(newVirtText, { suffix, "MoreMsg" })
+                return newVirtText
+            end
+
             local ufo = require("ufo")
 
             vim.keymap.set("n", "zK", ufo.peekFoldedLinesUnderCursor, { desc = "Peek Folded Lines" })
+            vim.keymap.set("n", "zR", ufo.openAllFolds, { desc = "Open all folds" })
+            vim.keymap.set("n", "zM", ufo.closeAllFolds, { desc = "Close all folds" })
+            vim.keymap.set("n", "zr", ufo.openFoldsExceptKinds, { desc = "Close folds with" })
+            vim.keymap.set("n", "zm", ufo.closeFoldsWith, { desc = "Close folds with" })
 
             ufo.setup({
                 provider_selector = function(_, filetype, _)
@@ -67,6 +100,7 @@ return {
 
                     return { "lsp", "indent" }
                 end,
+                fold_virt_text_handler = handler,
             })
         end,
     },
